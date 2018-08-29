7a339bb667dca76d7d1259f74adc320831781675
azizahhilda <hildazizah58@gmail.com>
2018-08-28 16:02:30 -1200

    bio

---
+++ bio.js
@@ -0,0 +1,15 @@
+let biodata = {
+    name: "Hilda Wahyu Azizah",
+    class: 11,
+    address: "Klapanunggal,Bogor",
+    age: 15,
+    school: "SMK Bina Mandiri Multimedia"
+
+}
+    console.log(
+        "Nama saya " + biodata.name +
+        "\nSaya kelas " + biodata.class +
+        "\nRumah saya di " + biodata.address +
+        "\nSaya berumur " + biodata.age + 
+        "\nSaya bersekolah di " + biodata.school 
+    )
